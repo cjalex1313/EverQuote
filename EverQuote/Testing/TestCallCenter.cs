af9248e50bcd72920d2c2691fc87ed2b503f5dbb
AlexCioarec <cioarec.alexandru96@gmail.com>
2020-06-23 01:58:21 +0300

    Call center tests

---
+++ EverQuote/Testing/TestCallCenter.cs
@@ -0,0 +1,30 @@
+﻿using EverQuote;
+using EverQuote.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing
+{
+    public class TestCallCenter
+    {
+
+        [Fact]
+        public void TestReceiveCall()
+        {
+            //arrange
+            var agents = new List<IAgent>();
+            var router = new Mock<IRouter>();
+            var callCenter = new CallCenter(router.Object, agents);
+            Consumer consumer = new Consumer(16, "Ro", 0, 0, false, 0, Guid.NewGuid());
+            //act
+            callCenter.ReceiveCall(consumer);
+
+            //assert
+            router.Verify(x => x.SelectMatchingAgent(consumer, agents), Times.Exactly(1));
+            Assert.Equal(callCenter.GetConsumer(consumer.Phone), consumer);
+        }
+    }
+}
