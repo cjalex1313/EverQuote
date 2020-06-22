965b96b762b4241d4bccad9bd31a6bc7c3ce73ff
AlexCioarec <cioarec.alexandru96@gmail.com>
2020-06-23 02:13:11 +0300

    Add agentMatching class

--- EverQuote/EverQuote/AgentMaching.cs
+++ EverQuote/EverQuote/AgentMaching.cs
@@ -1,6 +1,19 @@
-﻿namespace EverQuote
+﻿using System;
+
+namespace EverQuote
 {
     public class AgentMaching
     {
+        private readonly int[] _ageInterval;
+
+        public AgentMaching(int[] ageInterval)
+        {
+            this._ageInterval = ageInterval;
+        }
+
+        public bool IsMatching(Consumer consumer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
