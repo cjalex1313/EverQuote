e8f5760289d7e1fbba1799e0ede038c27d428ff8
AlexCioarec <cioarec.alexandru96@gmail.com>
2020-06-22 12:33:28 +0300

    Added consumer tests

---
+++ EverQuote/Testing/ConsumerTest.cs
@@ -0,0 +1,55 @@
+﻿using EverQuote;
+using EverQuote.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing
+{
+    public class ConsumerTest
+    {
+        [Fact]
+        public void TestConsumerIsNotHandled()
+        {
+            //arrange
+            Consumer consumer = new Consumer(16, "Ro", 0, 0, false, 0, Guid.NewGuid());
+            //act
+
+            //assert
+            Assert.False(consumer.IsHandeled);
+        }
+
+        [Fact]
+        public void TestConsumerIsHandledAfterCall()
+        {
+            //arrange
+            Consumer consumer = new Consumer(16, "Ro", 0, 0, false, 0, Guid.NewGuid());
+
+            //act
+            consumer.ReceiveCall();
+
+            //assert
+            Assert.True(consumer.IsHandeled);
+        }
+
+        [Fact]
+        public void TestConsumerCalledCallCenter()
+        {
+            //arrange
+            Consumer consumer = new Consumer(16, "Ro", 0, 0, false, 0, Guid.NewGuid());
+            var mockCenter = new Mock<ICallCenter>();
+            mockCenter.Setup(x => x.GetConsumer(consumer.Phone)).Returns(consumer);
+
+            //act
+            consumer.Call(mockCenter.Object);
+
+            //assert
+            var consumerInCenter = mockCenter.Object.GetConsumer(consumer.Phone);
+            Assert.Equal(consumerInCenter, consumer);
+        }
+
+
+    }
+}
