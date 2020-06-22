c0aac94ba4c3ec57883ca0f5d3c429b4c43678e4
AlexCioarec <cioarec.alexandru96@gmail.com>
2020-06-23 02:20:36 +0300

    Add agent matching tests

---
+++ EverQuote/Testing/AgentMatchingTest.cs
@@ -0,0 +1,28 @@
+﻿using EverQuote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing
+{
+    public class AgentMatchingTest
+    {
+        [Fact]
+        public void TestMatching()
+        {
+            //arrange
+            Consumer consumer = new Consumer(16, "Ro", 0, 0, false, 0, Guid.NewGuid());
+            var agentMatching1 = new AgentMatching(new int[] { 10, 20 });
+            var agentMatching2 = new AgentMatching(new int[] { 20, 30 });
+
+            //act
+            var result1 = agentMatching1.IsMatching(consumer);
+            var result2 = agentMatching2.IsMatching(consumer);
+
+            //assert
+            Assert.True(result1);
+            Assert.False(result2);
+        }
+    }
+}
