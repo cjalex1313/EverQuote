1f446c10cd7aef694a112f4aa92761fdebbf85a8
AlexCioarec <cioarec.alexandru96@gmail.com>
2020-06-22 12:31:49 +0300

    Added CallCenter Interface

---
+++ EverQuote/EverQuote/Interfaces/ICallCenter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EverQuote.Interfaces
+{
+    public interface ICallCenter
+    {
+        public void ReeciveCall(Consumer consumer);
+        public void AddAgent(Agent anget);
+        public Consumer GetConsumer(Guid phoneNumber);
+    }
+}
