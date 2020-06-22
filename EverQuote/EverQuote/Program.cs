d3125624de3c53448f0e8efdc041249cd08387ff
AlexCioarec <cioarec.alexandru96@gmail.com>
2020-06-22 10:13:34 +0300

    Create project

---
+++ EverQuote/EverQuote/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EverQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
