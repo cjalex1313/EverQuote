bcdfa4cdcf61a956e946e5e656a051b8712f4f4a
AlexCioarec <cioarec.alexandru96@gmail.com>
2020-06-23 04:05:02 +0300

    Create agent tests

---
+++ EverQuote/Testing/AgentTest.cs
@@ -0,0 +1,40 @@
+﻿using EverQuote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing
+{
+    public class AgentTest
+    {
+        [Fact]
+        public void TestReceiveCall()
+        {
+            //arrange
+            Consumer consumer = new Consumer(16, "Ro", 0, 0, false, 0, Guid.NewGuid());
+            var agent = new Agent(new AgentMatching(new int[] { 10, 20 }));
+            //act
+            agent.ReceiveCall(consumer);
+
+            //assert
+            Assert.Equal(agent.OnHoldQueue.Count, 1);
+        }
+
+        [Fact]
+        public void TestCallConsumer()
+        {
+            //arrage
+            Consumer consumer = new Consumer(16, "Ro", 0, 0, false, 0, Guid.NewGuid());
+            var agent = new Agent(new AgentMatching(new int[] { 10, 20 }));
+            agent.ReceiveCall(consumer);
+            //act
+            while (!consumer.IsHandeled)
+            {
+                agent.CallConsumer(consumer);
+            }
+            //assert
+            Assert.True(consumer.IsHandeled);
+        }
+    }
+}
