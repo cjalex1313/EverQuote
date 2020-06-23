90ac76b45952117f3484eae93faa9f79d425d2e1
AlexCioarec <cioarec.alexandru96@gmail.com>
2020-06-23 14:26:21 +0300

    Update tests for AgentMatching

--- EverQuote/Testing/AgentMatchingTest.cs
+++ EverQuote/Testing/AgentMatchingTest.cs
@@ -13,8 +13,8 @@ namespace Testing
         {
             //arrange
             Consumer consumer = new Consumer(16, "Ro", 0, 0, false, 0, Guid.NewGuid());
-            var agentMatching1 = new AgentMatching(new int[] { 10, 20 });
-            var agentMatching2 = new AgentMatching(new int[] { 20, 30 });
+            var agentMatching1 = new AgentMatching(new int[] { 10, 20 }, "Ro", 0,0, false, new int[]{0,100});
+            var agentMatching2 = new AgentMatching(new int[] { 20, 30 }, "Ro", 0,0, false, new int[] { 0, 100 });
 
             //act
             var result1 = agentMatching1.IsMatching(consumer);
